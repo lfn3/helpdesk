7871c4d016c35e0b65c655958396f608b0d9d096
byAtlas <atlas@byatlas.com>
2013-04-10 08:55:36 +1200

    Improvements to customer end forms.
    Little bit hacky at this stage, but I'll clean it up later.

--- survey/views.py
+++ survey/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render, get_object_or_404
 from django.forms.models import model_to_dict
 from django.utils.timezone import utc
+from django.contrib import messages
 
 from models import SurveyPub, SurveyPriv
 
@@ -24,8 +25,20 @@ def post(request, code):
     pub = priv.pub
     pub_dict = model_to_dict(pub, exclude=['id', 'priv'])
 
+    missing_vals = False
+
     for key in pub_dict:
-        pub_dict[key] = request.POST[key]
+        pub_dict[key] = request.POST.get(key, None)
+
+        if pub_dict[key] is None:
+            if 'rating' in key:
+                messages.error(request, "You haven't let us know what you think of our customer service.")
+            elif 'first_time_res' in key:
+                messages.error(request, "Please let us know if we resolved your issue the first time you contacted us.")
+            missing_vals = True
+
+    if missing_vals:
+        return render(request, 'survey/' + pub.TEMPLATE, {'code': code})
     
     pub = pub.__class__(id=pub.id, priv=pub.priv, **pub_dict)
     pub.save()
